77b67a44a031a497c3bbd3fc4bfe331ac1f0dee3
unknown <templanzatintin@gmail.com>
2023-04-17 22:52:05 +0800

    Greeting is working

---
+++ Decryption.py
@@ -0,0 +1,9 @@
+# Templanza, Kristine Joy F.
+# BSCPE 1-4
+# Assignment No.2 - Problem 2
+
+# Greeting and border line
+print("")
+print("\033[35m※ \033[0m" * 35)
+print("")
+print("\033[45m ♥ Welcome to our program! ♥ \033[0m".center(78))
