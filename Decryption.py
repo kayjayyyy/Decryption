a734d999ea1140e711024b01404e23e7ea577f02
unknown <templanzatintin@gmail.com>
2023-04-17 23:00:57 +0800

    User's message

--- Decryption.py
+++ Decryption.py
@@ -14,3 +14,7 @@ print("\n\033[3;33mI hope you are doing well,", name + "!\033[0m")
 print("")
 
 print("\033[36m Let's get started! \033[0m".center(78, "~"))
+
+# Ask the user to input the encrypted message
+encrypted_message = input("\n\033[3;35mPlease enter the message you want to decrypt: \033[0m")
+decrypted_message = ""
