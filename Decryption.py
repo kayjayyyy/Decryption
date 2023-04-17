399a8e8d7e0d508c43d54420b788d8391c7ff880
unknown <templanzatintin@gmail.com>
2023-04-17 22:59:00 +0800

    banner is working

--- Decryption.py
+++ Decryption.py
@@ -12,3 +12,5 @@ print("\033[45m ♥ Welcome to our program! ♥ \033[0m".center(78))
 name = input("\n\033[033mGood day! What is your name? \033[0m")
 print("\n\033[3;33mI hope you are doing well,", name + "!\033[0m")
 print("")
+
+print("\033[36m Let's get started! \033[0m".center(78, "~"))
